42063581a361278a80c0271a047d984712090ae3
Win <protwumasi@gmail.com>
2025-02-27 17:32:24 +0000

    initial commit

---
+++ src/main/java/com/dexwin/currencyconverter/controller/CurrencyController.java
@@ -0,0 +1,29 @@
+package com.dexwin.currencyconverter.controller;
+
+import com.dexwin.currencyconverter.service.CurrencyService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/")
+@RestController
+public class CurrencyController {
+
+    private final CurrencyService currencyService;
+
+    public CurrencyController(final CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    @GetMapping(value = "currencies/convert", produces = MediaType.APPLICATION_JSON_VALUE)
+    public double convert(
+            @RequestParam("source") String source,
+            @RequestParam("target") String target,
+            @RequestParam("amount") double amount) {
+
+        return currencyService.convert(source, target, amount);
+    }
+
+}
